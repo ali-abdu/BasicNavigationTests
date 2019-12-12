609245c7508a99241f67990ad43e5dcac25ac8d6
ali-abdu <ali.abdullayev-@hotmail.com>
2019-12-12 11:27:56 -0500

    'n'

---
+++ src/test/java/com/cbt/tests/Homework4/ff.java
@@ -0,0 +1,8 @@
+package com.cbt.tests.Homework4;
+
+public class ff {
+    public static void main(String[] args) {
+        System.out.println("ali abd");
+        System.out.println("tabriz");
+    }
+}

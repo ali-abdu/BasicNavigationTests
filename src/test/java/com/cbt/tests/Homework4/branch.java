a8ddb4496c1c5c8c49b4e47c197e16a8dad979df
ali-abdu <ali.abdullayev-@hotmail.com>
2019-12-12 11:50:13 -0500

    'new'

---
+++ src/test/java/com/cbt/tests/Homework4/branch.java
@@ -0,0 +1,12 @@
+package com.cbt.tests.Homework4;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import org.w3c.dom.ls.LSOutput;
+
+public class branch {
+
+    public static void main(String[] args) {
+        System.out.println("ali ");
+        System.out.println("abdullayev");
+    }
+}
